0ace72786bb927d96ba07fee5761b7b3ae14c16a
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-04-14 18:19:00 +0530

    Database setup for Test 1

---
+++ Test1/Data/EmployeeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Test1.Models;
+
+namespace Test1.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext() : base("EmployeeConnection") { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Test1/Migrations/202504141247162_EmployeeTable.cs
@@ -0,0 +1,28 @@
+﻿namespace Test1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmployeeTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        DOB = c.DateTime(nullable: false),
+                        Age = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
