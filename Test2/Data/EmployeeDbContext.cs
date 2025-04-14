866d018a4ff06179f73ca50dc617c36c352b9aea
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-04-14 18:54:40 +0530

    Database setup for Test 2

---
+++ Test2/Data/EmployeeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Test2.Models;
+
+namespace Test2.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext() : base("EmployeeConnection") { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Designation> Designations { get; set; }
+    }
+}
---
+++ Test2/Models/Designation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Test2.Models
+{
+    public class Designation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string DesignationName { get; set; }
+
+        // Navigation property
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
